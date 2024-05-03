154a1564bd728fc3099ece1f3d69fb13249c7a58
artus25200 <artus25200@gmail.com>
2024-05-03 20:01:12 +0200

    commit

--- src/avge.h
+++ src/avge.h
@@ -1,4 +1,24 @@
+/*
+**   Artus25200's Vulkan Game Engine (AVGE)
+*/
+
 #ifndef AVGE_H_
 #define AVGE_H_
+#include "nicelog.h"
+
+typedef enum { AVGE_ERROR, AVGE_WARN, AVGE_OK } AVGEStatus;
+
+typedef struct {
+
+} Renderer;
+
+typedef struct {
+  char *name;
+  char *version;
+  Renderer renderer;
+  Logger logger;
+} App;
+
+AVGEStatus AVGE_initialize_application(App *app, char *name, char *version);
 
 #endif // AVGE_H_
